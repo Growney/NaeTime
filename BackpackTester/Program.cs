9c7215695f8e31ec9cbc319a15a84e318e5755c3
Graeme Rowney <growney@tensor.co.uk>
2025-05-12 23:49:55 +0100

    Added starting connection

--- BackpackTester/Program.cs
+++ BackpackTester/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ELRS.Backpack;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
+using System;
 
  byte[] HashPhrase(string bindPhrase)
  {
@@ -23,10 +25,26 @@ using System.Text;
      return bindingPhraseHash;
  }
 
+Console.WriteLine("Creating connections");
+
 var serialiser = new BackpackCommandSerializer();
 var backpackConnectionFactory = new BackpackConnectionFactory(serialiser);
 var connection = backpackConnectionFactory.Create("COM7");
 
-byte[] uId = HashPhrase("bindingphrasehere");
+var cancellationTokenSource = new CancellationTokenSource();
 
+Console.WriteLine("Starting connections");
+await connection.ConnectAsync(cancellationTokenSource.Token);
+
+Console.WriteLine("Running connections");
+var running = connection.Run(cancellationTokenSource.Token);
+byte[] uId = HashPhrase("bindphrasehere");
+
+Console.WriteLine("Setting OSD");
 await connection.SetOSDElement(uId,"HELLO", OSDPresentation.Info, 3,0,TimeSpan.FromSeconds(5));
+
+Console.WriteLine("Stopping connection");
+cancellationTokenSource.Cancel();
+
+Console.WriteLine("Waiting For Stop");
+await running;
