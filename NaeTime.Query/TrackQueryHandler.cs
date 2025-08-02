e18555d7c79be8ccc9c8731f96185c4d183e777e
Graeme Rowney <growney@tensor.co.uk>
2025-08-02 08:50:16 +0100

    Added I query handler

---
+++ NaeTime.Query/TrackQueryHandler.cs
@@ -0,0 +1,18 @@
+using NaeTime.Query.Abstractions;
+using NaeTime.Query.Abstractions.Models;
+using NaeTime.Query.Projections;
+
+namespace NaeTime.Query;
+
+public class TrackQueryHandler : ITrackQueryHandler
+{
+    private readonly TrackList _trackList;
+
+    public TrackQueryHandler(TrackList trackList)
+    {
+        _trackList = trackList;
+    }
+
+    public Task<Track?> GetTrack(Guid id)
+        => Task.FromResult(_trackList.GetTrack(id));
+}
