aa95b86fdb3ec266a34431af8059e93fc527f352
Graeme Rowney <growney@tensor.co.uk>
2025-10-01 19:23:05 +0100

    Timing events

---
+++ NaeTime.Events/TimingEvents.cs
@@ -0,0 +1,4 @@
+﻿namespace NaeTime.Events;
+public record HardwareDetectionOccured(Guid TimerId, byte Lane, ulong? HardwareTime, long SoftwareTime, DateTime UtcTime);
+public record DetectionTriggered(byte Lane, ulong? HardwareTime, long SoftwareTime, DateTime UtcTime);
+public record DetectionAssignedToSession(Guid SessionId, byte Lane, ulong? HardwareTime, long SoftwareTime, DateTime UtcTime);
