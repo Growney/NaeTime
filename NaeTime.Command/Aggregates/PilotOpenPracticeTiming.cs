aa1eb2816b50db7192a6f271eba8f2000fc533eb
Graeme Rowney <rowney.graeme@googlemail.com>
2025-10-04 12:30:28 +0100

    Pilot events

---
+++ NaeTime.Command/Aggregates/PilotOpenPracticeTiming.cs
@@ -0,0 +1,20 @@
+﻿using EventDbLite.Aggregates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaeTime.Command.Aggregates;
+public class PilotOpenPracticeTiming: AggregateRoot<string>
+{
+    public PilotOpenPracticeTiming(Guid pilotId, Guid sessionId)
+    {
+        Raise(new Events.PilotOpenPracticeSessionTimingStarted(pilotId, sessionId));
+    }
+
+    private void When(Events.PilotOpenPracticeSessionTimingStarted e)
+    {
+        Id = $"{e.PilotId}-{e.SessionId}";
+    }
+}
---
+++ NaeTime.Events/PilotOpenPracticeEvents.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaeTime.Events;
+public record PilotOpenPracticeSessionTimingStarted(Guid PilotId, Guid SessionId);
+public record OpenPracticeDetectionAssignedToPilot(Guid DetectionId, Guid PilotId, Guid SessionId, ulong? HardwareTime, long SoftwareTime, DateTime UtcTime);
+
