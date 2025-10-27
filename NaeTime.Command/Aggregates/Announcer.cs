be51ae56172c4190293b51dd40b10f0256d3aaf7
Graeme Rowney <rowney.graeme@googlemail.com>
2025-10-27 21:02:57 +0000

    Initial announcements

---
+++ NaeTime.Command/Aggregates/Announcer.cs
@@ -0,0 +1,28 @@
+﻿using EventDbLite.Aggregates;
+using NaeTime.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaeTime.Command.Aggregates;
+public class Announcer : AggregateRoot<Guid>
+{
+    public Announcer(Guid id,string name)
+    {
+        Raise(new AnnouncerConfigurationCreated(id, name));
+
+    }
+
+    private void When(AnnouncerConfigurationCreated created)
+    {
+        Id = created.AnnouncerId;
+    }
+
+    public void Rename(string newName)
+    {
+        Raise(new AnnouncerConfigurationRenamed(Id, newName));
+    }
+
+}
---
+++ NaeTime.Events/AnnouncerConfigurationEvents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaeTime.Events;
+public record AnnouncerConfigurationCreated(Guid AnnouncerId, string Name);
+public record AnnouncerConfigurationRenamed(Guid AnnouncerId, string NewName);
+public record AnnouncerOpenPracticeDetectionAnnouncementEnabled(Guid AnnouncerId);
+public record AnnouncerOpenPracticeDetectionAnnouncementDisabled(Guid AnnouncerId);
+public record AnnouncerOpenPracticeDetectionPilotNamePrioritySet(Guid AnnouncerId, int priority);
+public record AnnouncerOpenPracticeDetectionPilotLatestLapDurationPrioritySet(Guid AnnouncerId, int priority);
+public record AnnouncerOpenPracticeDetectionPilotLatestSingleLapDurationPrioritySet(Guid AnnouncerId, int priority);
+public record AnnouncerOpenPracticeDetectionPilotLatestConsecutiveLapDurationPrioritySet(Guid AnnouncerId, uint LapCount, int priority);
+public record AnnouncerOpenPracticePilotFrequencyAssignedAnnouncementEnabled(Guid AnnouncerId);
+public record AnnouncerOpenPracticePilotFrequencyAssignedAnnouncementDisabled(Guid AnnouncerId);
+
+public record AnnouncerTimerConnectionStatusAnnouncementEnabled(Guid AnnouncerId);
+public record AnnouncerTimerConnectionStatusAnnouncementDisabled(Guid AnnouncerId);
+public record AnnouncerTimerLaneMismatchAnnouncementEnabled(Guid AnnouncerId);
+public record AnnouncerTimerLaneMismatchAnnouncementDisabled(Guid AnnouncerId);
