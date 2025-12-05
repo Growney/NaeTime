819cea302ddab5a061b918ca6a5378663fddaf8c
Graeme Rowney <rowney.graeme@googlemail.com>
2025-12-05 22:32:45 +0000

    Not working conversion to client side

--- NaeTime.Client.BlazorWebApp/NaeTime.Client.BlazorWebApp.Client/Program.cs
+++ NaeTime.Client.BlazorWebApp/NaeTime.Client.BlazorWebApp.Client/Program.cs
@@ -1,7 +1,27 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddHttpClient(string.Empty,x=>
+{
+    x.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+});
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+builder.Services.AddSpeechSynthesis();
 builder.Services.AddApiClients();
 builder.Services.AddEventDbSignalRReactions(builder.HostEnvironment.BaseAddress);
 
